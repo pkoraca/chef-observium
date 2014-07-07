48ce2568df24358b0231f343edea13a82251d4dc
Petar Koraca <Petar.Koraca@infobip.com>
2014-07-07 21:49:46 +0200

    Added apt dependency and kitchen file

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures Observium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.2'
 
-%w{ark cron apache2 mysql yum yum-epel yum-repoforge}.each do |pkg|
+%w{apt ark cron apache2 mysql yum yum-epel yum-repoforge}.each do |pkg|
   depends pkg
 end
 
