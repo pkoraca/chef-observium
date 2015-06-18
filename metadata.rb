96a46f15998d9073d6541dd940508c92cc50670b
Julian Tabel <j.tabel@bigpoint.net>
2015-06-18 14:56:52 +0200

    * new community release changes some settings in config.php

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'pkoraca@gmail.om'
 license 'Apache'
 description 'Installs/Configures Observium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.2'
+version '0.1.3'
 
 %w(yum yum-epel yum-repoforge ark cron apache2 mysql database).each do |pkg|
   depends pkg
