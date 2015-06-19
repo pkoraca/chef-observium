d6617782b48409496644d1a69a1f83c86627dbca
Julian Tabel <j.tabel@bigpoint.net>
2015-06-19 16:38:38 +0200

    * pinned mysql cookbook due to major change in version 6

--- metadata.rb
+++ metadata.rb
@@ -6,9 +6,11 @@ description 'Installs/Configures Observium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.5'
 
-%w(yum yum-epel yum-repoforge ark cron apache2 mysql database).each do |pkg|
+%w(yum yum-epel yum-repoforge ark cron apache2 database).each do |pkg|
   depends pkg
 end
+
+depends 'mysql', '~> 5'
 
 %w(redhat centos ubuntu debian).each do |os|
   supports os
